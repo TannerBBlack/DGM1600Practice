ae28b233c5581e9c2f44045c359cd917fff2c8d2
Tanner Black <tbb777@gmail.com>
2019-11-21 18:18:43 -0700

    saving progress on stats

--- PracticePortfolio/build_card_display.js
+++ PracticePortfolio/build_card_display.js
@@ -1,6 +1,7 @@
 import { starships } from "./starships.js";
 //console.log("Starship list", starships);
 var containerDiv = document.getElementById('cardContainer');
+var shipSelectorDiv = document.getElementById('shipselector');
 
 starships.forEach(function (starship, index) {
     if (index < 25) {
@@ -24,7 +25,7 @@ function addStarshipCard(starship, index) {
     var cardBack = document.createElement('DIV');   //creates a <div></div>
     cardFront.setAttribute("class", 'card__face card__face--front');   //sets the class for the <div class="card__face card__face--front"></div>
     cardBack.setAttribute("class", 'card__face card__face--back');   //sets the class for the <div class="card__face card__face--back"></div>
-    cardBack.innerHTML = starship.name;   //takes the starship name and displays it <div class="card__face card__face--back">starship name</div>
+    cardBack.appendChild(addStarshipStats(starship))  //takes the starship name and displays it <div class="card__face card__face--back">starship name</div>
     cardFront.appendChild(addStarshipImage(starship));  //adds the starship image to the front <div class="card__face card__face--front"><img src="starshipapiindex"></div>
     cardDiv.appendChild(cardFront);   //adds cardFront to cardDiv <div class="card" id="card + index"><div class="card__face card__face--front"><img src="starshipapiindex"></div></div> 
     cardDiv.appendChild(cardBack);    //adds cardBack to cardDiv <div class="card" id="card + index"><div class="card__face card__face--front"><img src="starshipapiindex"></div><div class="card__face card__face--back">starship name</div></div> 
@@ -51,3 +52,30 @@ function addFlipListeners(index) {
        card.classList.toggle('is-flipped');
    })
 }    
+
+function addStarshipStats(starship) {
+    var starshipStats = document.createElement('DIV');
+    var starshipName = document.createElement("H3");
+    var starshipCost = document.createElement("P");
+    var starshipCrew = document.createElement("P");
+    var starshipClass = document.createElement("P");
+    starshipName.innerHTML = starship.name;
+    starshipCost.innerHTML = "Cost: " + starship.cost_in_credits;
+    starshipCrew.innerHTML = "Crew: " + starship.crew;
+    starshipClass.innerHTML = "Class: " + starship.starship_class;
+    starshipStats.appendChild(starshipName);
+    starshipStats.appendChild(starshipCost);
+    starshipStats.appendChild(starshipCrew);
+    starshipStats.appendChild(starshipClass);
+    return starshipStats;
+}
+
+function addNewStarshipSelector() {
+
+}
+
+//<div>
+//  <p>Cost: </p>
+//  <p>Crew: </p>
+//  <p>Class: </p>
+//</div>
